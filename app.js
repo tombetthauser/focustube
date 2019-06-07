6d7ef622ce1d309f0b6f249f3c9a4bd49a46cb2f
Tom Betthauser <tombetthauser@gmail.com>
2019-06-07 13:01:39 -0700

    cleaned up text js

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-const mysteryVideos = [
-	"aGV_eeAvvzM",
-	"GxYyuaTsVts",
-	"-UYgORr5Qhg&t=0m57s",
-	"_sh6MDFJdNU",
-	"hqega6a2g6A",
-	"D5txvDBNgYc",
-	"TCOOfNraqYw",
-	"1XHTW79Qa6Y"
-	// https://www.youtube.com/watch?v=
-	// http://www.infinitelooper.com/?v=
-];
-const placeholderArray = [
-	"focus, don't get distracted...",
-	"you can do it, focus...",
-	"for a more productive tube...",
-	"don't lose focus...",
-	"focus, stay on target...",
-	"don't get lost in the tube...",
-	"tube in and tube out...",
-	"watch out for the tube...",
-	"the tube is coming for you...",
-	"don't fall into the tube...",
-	"bypass the tube tunnel...",
-	"it's time to tube better...",
-	"a better way to tube...",
-	"the productive way to tube...",
-	"maintain tube focus...",
-	"don't let the tube get you...",
-	"rise above the tube...",
-	"escape the tube...",
-	"tube in focus..."
-];
-const warningsOne = [
-	"you have to type something first...",
-	"uh, you have to type something...",
-	"you can't search for nothing...",
-	"you didn't type anything yet...",
-	"so, first type something..."
-];
-const warningsTwo = [
-	"cut it out...",
-	"you wont be warned again...",
-	"come on, type something...",
-	"you still didn't type anything...",
-	"no, type something..."
-];
-const warningsThree = [
-	"alright fine, do it again...",
-	"ok then, do it again...",
-	"fine, do it one more time...",
-	"ok do it again...",
-	"well, keep going then..."
-];
+const text = {
+	mysteryVideos: [
+		"aGV_eeAvvzM",
+		"GxYyuaTsVts",
+		"-UYgORr5Qhg&t=0m57s",
+		"_sh6MDFJdNU",
+		"hqega6a2g6A",
+		"D5txvDBNgYc",
+		"TCOOfNraqYw",
+		"1XHTW79Qa6Y"
+	], 
+	placeholderArray: [
+		"focus, don't get distracted...",
+		"you can do it, focus...",
+		"for a more productive tube...",
+		"don't lose focus...",
+		"focus, stay on target...",
+		"don't get lost in the tube...",
+		"tube in and tube out...",
+		"watch out for the tube...",
+		"the tube is coming for you...",
+		"don't fall into the tube...",
+		"bypass the tube tunnel...",
+		"it's time to tube better...",
+		"a better way to tube...",
+		"the productive way to tube...",
+		"maintain tube focus...",
+		"don't let the tube get you...",
+		"rise above the tube...",
+		"escape the tube...",
+		"tube in focus..."
+	],
+	warningsOne: [
+		"you have to type something first...",
+		"uh, you have to type something...",
+		"you can't search for nothing...",
+		"you didn't type anything yet...",
+		"so, first type something..."
+	],
+	warningsTwo: [
+		"cut it out...",
+		"you wont be warned again...",
+		"come on, type something...",
+		"you still didn't type anything...",
+		"no, type something..."
+	],
+	warningsThree: [
+		"alright fine, do it again...",
+		"ok then, do it again...",
+		"fine, do it one more time...",
+		"ok do it again...",
+		"well, keep going then..."
+	]
+};
 const coll = document.getElementsByClassName("collapsible");
 let i;
 let randomVideoCounter = 0;
@@ -60,22 +60,22 @@ const searchFunction = () => {
 	if (inputValue === "") {
 		randomVideoCounter ++;
 		if (randomVideoCounter === 1) {
-			let randomArrayNumber = Math.round(Math.random() * (warningsOne.length - 1));
-			document.getElementById('input').placeholder = warningsOne[randomArrayNumber];
+			let randomArrayNumber = Math.round(Math.random() * (text.warningsOne.length - 1));
+			document.getElementById('input').placeholder = text.warningsOne[randomArrayNumber];
 		} else if (randomVideoCounter === 2) {
-			let randomArrayNumber = Math.round(Math.random() * (warningsTwo.length - 1));
-			document.getElementById('input').placeholder = warningsTwo[randomArrayNumber];
+			let randomArrayNumber = Math.round(Math.random() * (text.warningsTwo.length - 1));
+			document.getElementById('input').placeholder = text.warningsTwo[randomArrayNumber];
 		} else if (randomVideoCounter === 3) {
-			let randomArrayNumber = Math.round(Math.random() * (warningsThree.length - 1));
-			document.getElementById('input').placeholder = warningsThree[randomArrayNumber];
+			let randomArrayNumber = Math.round(Math.random() * (text.warningsThree.length - 1));
+			document.getElementById('input').placeholder = text.warningsThree[randomArrayNumber];
 		} else {
-			let randomArrayNumber = Math.round(Math.random() * (mysteryVideos.length - 1));
+			let randomArrayNumber = Math.round(Math.random() * (text.mysteryVideos.length - 1));
 			clearInput();
 			let isChecked = document.getElementById("checkBox").checked;
 			if (isChecked === false) {
-				window.location.href = ("https://www.youtube.com/watch?v=" + mysteryVideos[randomArrayNumber]);
+				window.location.href = ("https://www.youtube.com/watch?v=" + text.mysteryVideos[randomArrayNumber]);
 			} else {
-				window.location.href = ("http://www.infinitelooper.com/?v=" + mysteryVideos[randomArrayNumber]);
+				window.location.href = ("http://www.infinitelooper.com/?v=" + text.mysteryVideos[randomArrayNumber]);
 			};
 		};
 	} else {
@@ -103,8 +103,8 @@ const searchFunction = () => {
 };
 const resetInput = () => {
 	document.getElementById('input').value = "";
-	let randomArrayNumber = Math.round(Math.random() * (placeholderArray.length - 1));
-	document.getElementById('input').placeholder = placeholderArray[randomArrayNumber];
+	let randomArrayNumber = Math.round(Math.random() * (text.placeholderArray.length - 1));
+	document.getElementById('input').placeholder = text.placeholderArray[randomArrayNumber];
 };
 const clearInput = () => {
 	document.getElementById('input').value = "";
